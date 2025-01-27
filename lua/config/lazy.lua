0fb2e306840b6c05e16a8069a4d7ec973140f32f
Hasibur Rahman <hasib.webdev@gmail.com>
2025-01-27 20:40:47 +0600

    Added which key and transparent tokyonight

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -15,6 +15,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+
 -- Setup lazy.nvim
 require("lazy").setup({
 	spec = {
@@ -50,3 +51,6 @@ require("lazy").setup({
 		},
 	},
 })
+
+
+vim.cmd([[colorscheme tokyonight]])
