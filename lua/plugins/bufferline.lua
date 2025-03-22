767d693afc6830247e44ff15d6dd75602e7d1370
Hasibur Rahman <hasib.webdev@gmail.com>
2025-03-22 09:03:31 +0600

    Updated

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -8,14 +8,6 @@ return {
 			separator_style = "slant",
 			show_buffer_close_icons = false,
 			show_close_icon = false,
-			offsets = {
-				{
-					filetype = "NvimTree",
-					text = "File Explorer",
-					highlight = "Directory",
-					separator = true,
-				},
-			},
 			diagnostics_indicator = function(count, level)
 				local icon = level:match("error") and " " or " "
 				return " " .. icon .. count
